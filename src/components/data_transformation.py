f6723ebaedae6b7c53f1936232cf9c6b8e28123d
aravint-annamalai <aravint2001@gmail.com>
2023-07-26 23:19:22 +0530

    Slight changes

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -47,7 +47,7 @@ class DataTransformation:
                     ("imputer", SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder", OneHotEncoder()),
                     ("scaler", StandardScaler(with_mean=False))
-                ]
+                ] 
             )
 
             logging.info(f"Categorical columns: {categorical_columns}")
